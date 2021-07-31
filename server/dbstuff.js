6c49642af718e199b623617ec4888e5dfd39e700
andrewhong5297 <47720952+andrewhong5297@users.noreply.github.com>
2021-07-31 13:05:52 -0400

    db fixed

--- server/dbstuff.js
+++ server/dbstuff.js
@@ -1,6 +1,10 @@
 const sqlite3 = require('sqlite3').verbose(); 
 const express = require("express");
+const bp = require('body-parser')
+
 const app = express();
+app.use(bp.json()) //idk why it says deprecated, someone let me know in issue or PR
+app.use(bp.urlencoded({ extended: true }))
 
 const HTTP_PORT = 8000
 app.listen(HTTP_PORT, () => {
@@ -9,7 +13,7 @@ app.listen(HTTP_PORT, () => {
 
 const db = new sqlite3.Database('./db/artblocks.db', (err) => {
     if (err) {
-        console.error("Erro opening database " + err.message);
+        console.error("Error opening database " + err.message);
     } else {
 
         db.run('CREATE TABLE artblock_mints( \
@@ -24,6 +28,7 @@ const db = new sqlite3.Database('./db/artblocks.db', (err) => {
             if (err) {
                 console.log("Table already exists.");
             }
+            // testing purposes before you setup get/post stuff
             // let insert = 'INSERT INTO artblock_mints (tx_hash, status, sender, projectId, gas_limit, gas_price) VALUES (?,?,?,?,?,?)';
             // db.run(insert, ["0x67b77db0d5022a91a46095cbdeba82deef8337b90e12f5171a830ef50cfb465d", "successful", "0xa55E01a40557fAB9d87F993d8f5344f1b2408072", "120", "1397818","10"]);
             // db.run(insert, ["0x67b77db0d5022a91a46095cbdeba82deef8337b90e12f5171a830ef50cfb465d", "cancelled", "0xa55E01a40557fAB9d87F993d8f5344f1b2408072", "120", "1397818","10"]);
@@ -37,7 +42,8 @@ app.get("/", (req, res, next) => {
 })
 
 app.post("/newMint/", (req, res, next) => {
-    var reqBody = re.body;
+    const reqBody = req.body;
+    // console.log(reqBody)
     db.run(`INSERT INTO artblock_mints (tx_hash, status, sender, projectId, gas_limit, gas_price) VALUES (?,?,?,?,?,?)`,
         [reqBody.tx_hash, reqBody.status, reqBody.sender, reqBody.projectId, reqBody.gas_limit, reqBody.gas_price],
         function (err, result) {
