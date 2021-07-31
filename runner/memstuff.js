a2365704a09ff9048a61d577539c4f2566eb7ae4
andrewhong5297 <47720952+andrewhong5297@users.noreply.github.com>
2021-07-31 12:44:04 -0400

    updating readme

--- runner/memstuff.js
+++ runner/memstuff.js
@@ -4,13 +4,6 @@ const WebSocket = require('ws');
 const { config } = require("./configuration.json")
 require('dotenv').config()
 
-/*
-1. create a subscription and filters in mempool explorer, then download the config json file
-2. setup a webhook url here (https://webhook.site/#!/758be919-6f7a-4fa2-b22c-bef2290165ee) then go to accounts page and create a ws
-3. setup your sqlite db or whatever other preferred data storage you want 
-5. profit
-*/
-
 const handleTransactionEvent = event => {
   const {
     transaction, // transaction object
@@ -37,8 +30,8 @@ async function main() {
   await blocknative.configuration({
     scope: "0xa7d8d9ef8D8Ce8992Df33D8b8CF4Aebabd5bD270", // [required] - either 'global' or valid Ethereum address
     //filters: config.filters, // [optional] - array of valid searchjs filter strings
-    abi: config.abi, // [optional] - valid contract ABI
-    watchAddress: true // [optional] - Whether the server should automatically watch the "scope" value if it is an address
+    abi: config.abi // [optional] - valid contract ABI
+    // watchAddress: true // [optional] - Whether the server should automatically watch the "scope" value if it is an address
   })  
 }
 
--- server/dbstuff.js
+++ server/dbstuff.js
@@ -7,28 +7,47 @@ app.listen(HTTP_PORT, () => {
     console.log("Server is listening on port " + HTTP_PORT);
 });
 
-let db = new sqlite3.Database('./db/artblocks.db', sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database('./db/artblocks.db', (err) => {
     if (err) {
-      return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
-  });
+        console.error("Erro opening database " + err.message);
+    } else {
 
-//create table once, then insert into table after webhook each time
-// db.serialize(() => {
-// db.each(`SELECT PlaylistId as id,
-//                 Name as name
-//             FROM playlists`, (err, row) => {
-//     if (err) {
-//     console.error(err.message);
-//     }
-//     console.log(row.id + "\t" + row.name);
-// });
-// });
-
-db.close((err) => {
-    if (err) {
-      return console.error(err.message);
+        db.run('CREATE TABLE artblock_mints( \
+            event_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+            tx_hash NVARCHAR(66)  NOT NULL,\
+            status NVARCHAR(10)  NOT NULL,\
+            sender NVARCHAR(42) NOT NULL,\
+            projectId INTEGER NOT NULL,\
+            gas_limit INTEGER NOT NULL,\
+            gas_price INTEGER NOT NULL\
+        )', (err) => {
+            if (err) {
+                console.log("Table already exists.");
+            }
+            // let insert = 'INSERT INTO artblock_mints (tx_hash, status, sender, projectId, gas_limit, gas_price) VALUES (?,?,?,?,?,?)';
+            // db.run(insert, ["0x67b77db0d5022a91a46095cbdeba82deef8337b90e12f5171a830ef50cfb465d", "successful", "0xa55E01a40557fAB9d87F993d8f5344f1b2408072", "120", "1397818","10"]);
+            // db.run(insert, ["0x67b77db0d5022a91a46095cbdeba82deef8337b90e12f5171a830ef50cfb465d", "cancelled", "0xa55E01a40557fAB9d87F993d8f5344f1b2408072", "120", "1397818","10"]);
+            // db.run(insert, ["0x67b77db0d5022a91a46095cbdeba82deef8337b90e12f5171a830ef50cfb465d", "speed up", "0xa55E01a40557fAB9d87F993d8f5344f1b2408072", "120", "1397818","10"]);
+        });
     }
-    console.log('Close the database connection.');
-  });
+});
+
+app.get("/", (req, res, next) => { 
+    return res.status(200).send({ message: 'ok' });
+})
+
+app.post("/newMint/", (req, res, next) => {
+    var reqBody = re.body;
+    db.run(`INSERT INTO artblock_mints (tx_hash, status, sender, projectId, gas_limit, gas_price) VALUES (?,?,?,?,?,?)`,
+        [reqBody.tx_hash, reqBody.status, reqBody.sender, reqBody.projectId, reqBody.gas_limit, reqBody.gas_price],
+        function (err, result) {
+            if (err) {
+                res.status(400).json({ "error": err.message })
+                return;
+            }
+            res.status(201).json({
+                "event_id": this.lastID,
+                "tx_hash": reqBody.tx_hash
+            })
+        });
+});
